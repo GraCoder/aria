b4ad123fae1170eb6b74f58b3c01ac79593dde95
tcx <1753592@outlook.com>
2021-07-02 18:01:09 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   ui/imgui_dlg.cpp

--- ui/imgui_dlg.cpp
+++ ui/imgui_dlg.cpp
@@ -3,24 +3,24 @@
 #include <SDL2/SDL_vulkan.h>
 #include <vulkan/vulkan.h>
 
+#include <vector>
 
 ImGuiDlg::ImGuiDlg()
 {
 	SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
 	SDL_Window* window = SDL_CreateWindow("Dear ImGui SDL2+Vulkan example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
 
-	//uint32_t extensions_count = 0;
-	//SDL_Vulkan_GetInstanceExtensions(window, &extensions_count, NULL);
-	//const char** extensions = new const char* [extensions_count];
-	//SDL_Vulkan_GetInstanceExtensions(window, &extensions_count, extensions);
+	uint32_t extensions_count = 0;
+	SDL_Vulkan_GetInstanceExtensions(window, &extensions_count, NULL);
+	std::vector<const char*> extensions;
+	SDL_Vulkan_GetInstanceExtensions(window, &extensions_count, extensions.data());
 	//SetupVulkan(extensions, extensions_count);
-	//delete[] extensions;
 
-	//// Create Window Surface
-	//VkSurfaceKHR surface;
-	//VkResult err;
-	//if (SDL_Vulkan_CreateSurface(window, g_Instance, &surface) == 0)
-	//{
-	//	printf("Failed to create Vulkan surface.\n");
-	//}
+	// Create Window Surface
+	VkSurfaceKHR surface;
+	VkResult err;
+	if (SDL_Vulkan_CreateSurface(window, g_Instance, &surface) == 0)
+	{
+		printf("Failed to create Vulkan surface.\n");
+	}
 }
